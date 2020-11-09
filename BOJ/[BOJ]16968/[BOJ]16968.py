f22f58b294d496e89106dbbb261eec4b01f50923
SongSeoYoung <bgd216@naver.com>
2020-11-09 13:15:30 +0900

    [add] BOJ 16968

--- BOJ/[BOJ]16968/[BOJ]16968.py
+++ BOJ/[BOJ]16968/[BOJ]16968.py
@@ -1,1 +1,26 @@
 ## 백준 16968
+
+n = input()
+n = list(n)
+
+sum = 1
+
+for i in range(len(n)):
+    if i==0:
+        if n[i]=="c":
+            sum *=26
+        else:
+            sum *=10
+    else:
+        if n[i]=="c":
+            if n[i-1] =="c":
+                sum *=25
+            else:
+                sum *=26
+        else:
+            if n[i-1] =="d":
+                sum *=9
+            else:
+                sum *=10
+
+print(sum)
