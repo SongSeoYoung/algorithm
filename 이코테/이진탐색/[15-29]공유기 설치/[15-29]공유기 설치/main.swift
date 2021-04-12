d886c9b41018afe16a1c9b9f6f2adb218343aaf0
seoyoung <seoyoung_00@hufs.ac.kr>
2021-04-13 01:30:33 +0900

    [이코테] 공유기설치

--- 이코테/이진탐색/[15-29]공유기 설치/[15-29]공유기 설치/main.swift
+++ 이코테/이진탐색/[15-29]공유기 설치/[15-29]공유기 설치/main.swift
@@ -1,11 +1,41 @@
-//
-//  main.swift
-//  [15-29]공유기 설치
-//
-//  Created by 송서영 on 2021/04/12.
-//
+func solution () {
+    let info = readLine()!.split(separator: " ").map { Int(String($0))! }
+    var arr = [Int]()
+    for _ in 0..<info[0] {
+        arr.append(Int(String(readLine()!))!)
+    }
+    arr.sort()
+    print(binarySearch(arr: arr, start: 0, end: arr.count - 1, install: info[1])!)
+}
 
-import Foundation
-
-print("Hello, World!")
+func binarySearch(arr: [Int], start: Int, end: Int, install: Int) -> Int? {
+    var minsize = arr[start + 1] - arr[start]
+    var maxsize = arr[end] - arr[start]
+    var answer = 0
+    while (minsize <= maxsize){
+        
+        let target = (minsize + maxsize ) / 2
+        var check = [0]
+        for i in 1..<arr.count {
+            if arr[i] >= arr[check.last!] + target {
+                check.append(i)
+            }
+        }
+        if check.count < install {
+            maxsize = target - 1
+        } else {
+            minsize = target + 1
+            answer = target
+        }
+    }
+    if answer == 0 {
+        var sub = [Int]()
+        for i in 1..<arr.count {
+            sub.append(arr[i] - arr[i-1])
+        }
+        return sub.min()
+    }
+    return answer
+}
+solution()
 
